70353b678bad4a7bc2975ef7c9d749f2d0d439c0
o-x-y-g-e-n <shivamgohel777@gmail.com>
2018-05-15 12:36:07 +0530

    google new gui added

--- fiverr.py
+++ fiverr.py
@@ -27,17 +27,22 @@ for service in service_list:
 	for town in town_list:
 		search_term = str(service) + " " + str(town)
 		f = open(search_term,"w+")
-		browser.get('https://www.google.co.in/search?q=&npsic=0&rflfq=1&rlha=0&rllag=51136382,869325,1970&tbm=lcl&ved=0ahUKEwjHk7T1lvTZAhVHYo8KHU1GAUgQjGoIaQ&tbs=lrf:!2m1!1e3!3sIAE,lf:1,lf_ui:2&rldoc=1#rlfi=hd:;si:;mv:!1m3!1d17051.630571922742!2d0.87223795!3d51.14550385!2m3!1f0!2f0!3f0!3m2!1i381!2i327!4f13.1;tbs:lrf:!2m1!1e3!3sIAE,lf:1,lf_ui:2')
+		browser.get('https://www.google.co.uk/search?q=&npsic=0&rflfq=1&rlha=0&rllag=51136382,869325,1970&tbm=lcl&ved=0ahUKEwjHk7T1lvTZAhVHYo8KHU1GAUgQjGoIaQ&tbs=lrf:!2m1!1e3!3sIAE,lf:1,lf_ui:2&rldoc=1#rlfi=hd:;si:;mv:!1m3!1d17051.630571922742!2d0.87223795!3d51.14550385!2m3!1f0!2f0!3f0!3m2!1i381!2i327!4f13.1;tbs:lrf:!2m1!1e3!3sIAE,lf:1,lf_ui:2')
 		s_b = browser.find_element_by_id('lst-ib')
 		browser.execute_script('arguments[0].innerHTML = "";',s_b)	
 		s_b.send_keys("")
 		s_b.send_keys(search_term)
 		s_b.send_keys(Keys.ENTER)
-		website = browser.find_elements_by_xpath("//a[@class='yYlJEf L48Cpd']")
-		if(website):
-			for e in website:
+		
+
+		website_tag = browser.find_elements_by_xpath("//div[@class='dbg0pd']")
+		if(website_tag):
+			for e in website_tag:
+				e.click()
+				time.sleep(3)
+				website = browser.find_element_by_xpath('//a[@class="LJOFid ab_button"]')
+				url = website.get_attribute('href')
 				emails = []
-				url = e.get_attribute('href')
 				emails = get_email(url)
 				if emails:
 					for email in emails:
